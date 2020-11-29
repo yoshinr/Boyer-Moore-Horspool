27393eb90580516e313ba69a1c0db60a5f5f6a36
Yoshinori <74362509+yoshinr@users.noreply.github.com>
2020-11-29 18:56:06 +0900

    getterとsetterを追加した

--- src/BoyerMooreHorspool.java
+++ src/BoyerMooreHorspool.java
@@ -2,31 +2,63 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class BoyerMooreHorspool {
-	
+
 	private char[] text, pat;
 	// 文字とそれに対応する移動できる文字数を持つ。
 	private Map<Character, Integer> skipTable = new HashMap<>();
-	
+
+	public BoyerMooreHorspool(char[] text, char[] pattern) {
+		setText(text);
+		setPattern(pattern);
+	}
+
 	public BoyerMooreHorspool(String text, String pattern) {
-		this.text = text.toCharArray();
-		this.pat = pattern.toCharArray();
+		setText(text);
+		setPattern(pattern);
+	}
+
+	public void setText(char[] text) {
+		if (text.length == 0) throw new IllegalArgumentException("textが0文字です。");
+		this.text = text;
+	}
+
+	public void setText(String text) {
+		setText(text.toCharArray());
+	}
+
+	public void setPattern(char[] pattern) {
+		if (pattern.length == 0) throw new IllegalArgumentException("patternが0文字です。");
+		this.pat = pattern;
 		makeSkipTable();
 	}
-	
+
+	public void setPattern(String pattern) {
+		setPattern(pattern.toCharArray());
+	}
+
+	public String getText() {
+		return text.toString();
+	}
+
+	public String getPattern() {
+		return pat.toString();
+	}
+
 	// ずらす量を表すテーブルを作る。
-	private void makeSkipTable() {
+	protected void makeSkipTable() {
+		skipTable.clear();
 		for (int i = 0; i < pat.length - 1; i++) {
-			this.skipTable.put(pat[i], pat.length - i - 1);
+			skipTable.put(pat[i], pat.length - i - 1);
 		}
 	}
-	
+
 	// パターンが見つからない場合は-1を返す。
 	public int search() {
 		int lastIndexOfText = pat.length - 1;
 		while (lastIndexOfText < text.length) {
 			int textI = lastIndexOfText;
 			int patI = pat.length - 1;
-			while(text[textI] == pat[patI]) {
+			while (text[textI] == pat[patI]) {
 				if (patI == 0) return textI; // パッターンに一致する部分が見つかったので、見つかった位置の添字を返す。
 				textI--;
 				patI--;
@@ -35,5 +67,5 @@ public class BoyerMooreHorspool {
 		}
 		return -1; // 見つからなかった。
 	}
-	
+
 }
--- src/Tester.java
+++ src/Tester.java
@@ -1,18 +1,24 @@
 
 public class Tester {
-	
+
 	public static void main(String[] args) {
+		// 作成日: 2020/11/29
+
 		String text = "abdabcfefe";
-		String pattern = "abc";
-		
+		String pattern = "ef";
+
 		BoyerMooreHorspool searcher = new BoyerMooreHorspool(text, pattern);
+
 		int index = searcher.search();
-		
-		if (index > 0) {
-			System.out.println("Found at index: " + index);
+		if (index >= 0) {
+			System.out.println("Found! index: " + index);
 		} else {
 			System.out.println("Not Found");
 		}
+		
+		// 実行結果
+		// Found! index: 7
+
 	}
-	
+
 }
